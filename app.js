4a8f96e05a4d5a7c99ea7b137719d9d1ae08cbdb
Sulaiman Hamouda <hamouda.sulaiman@gmail.com>
2019-05-09 15:06:41 -0400

    r

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const app = express();
 app.use(express.static("public"));
 app.use("/public/uploads", express.static("public/uploads"))
 app.set('view engine', 'ejs');
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
